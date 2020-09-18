a24a91ef49746e8c541102b1ad164bc379a63286
daylinjones <crescentwd@gmail.com>
2020-09-18 09:52:34 -0400

    API Update
    Files for family API added. Basic model created. Working on routes.

---
+++ app/models/family_api.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const familyMemberSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  relationship: {
+    type: String,
+    required: true
+  },
+  generation: {
+    type: Number,
+    required: true
+  },
+  familyTree: {
+    type: String,
+    required: true
+  }
+  // owner: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  //   required: true
+  // }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('FamilyMember', familyMemberSchema)
